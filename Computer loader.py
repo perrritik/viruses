d9bd76d12fb8a621abb64c42daf8f8b349bc317a
perrritik <143010263+perrritik@users.noreply.github.com>
2023-08-24 14:49:35 +0400

    Create Computer loader.py
    Version 0.0.1 BETA
    by perrritik

---
+++ Computer loader.py
@@ -0,0 +1,5 @@
+import webbrowser
+for i in range(15): #можно сделать и 15 и 1000 , ооочень нагружает комп.(если  у вас не i9 и 4090)
+    webbrowser.open("https://www.youtube.com/watch?v=RVnSo4Vuqv4")
+    webbrowser.open("https://www.google.com/search?q=by+perrritik%F0%9F%92%80&oq=by+perrritik%F0%9F%92%80&aqs=chrome..69i57j33i160.821j0j4&sourceid=chrome&ie=UTF-8")
+#Самый изичный нагружатель компов. Есть минусы, но все же я его написал за  пару минут.
